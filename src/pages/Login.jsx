70359e51496610c62cb9dca1edc5702f7ed15840
EmilianoColavita <colavitaemiliano1@gmail.com>
2025-11-17 13:04:05 -0300

    MOD(arreglo de login)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
     setError("");
 
     try {
-      const res = await api.post("/auth/login", { email, password });
+      const res = await api.post("/api/auth/login", { email, password });
       localStorage.setItem("token", res.data.token);
 
       alert("Inicio de sesión exitoso");
